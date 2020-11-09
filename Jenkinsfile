6f6a9c26b64315257e5885aae161be525f876f14
ashish_nargade <ashish_nargade@ARPGL000908.persistent.co.in>
2020-11-09 11:19:56 +0530

    Added arguments in jenkinsfile and main.py

--- Jenkinsfile
+++ Jenkinsfile
@@ -4,7 +4,7 @@ pipeline {
 	stages{
 		stage ('Test Stage') {
 			steps {
-				sh 'python main.py'
+				sh 'python main.py ${Env} ${Lambda}'
 			}
 		}
 	}	
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
+import sys
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(env,lambda):
     # Use a breakpoint in the code line below to debug your script.
     print('Hi, There')
-
+	print 'Arguments from the Jenkins Job: %s, %s' % (str(sys.argv[1]), str(sys.argv[2]))
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
