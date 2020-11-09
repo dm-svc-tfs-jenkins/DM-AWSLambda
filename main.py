0d11e953a8445c9c742d1f31541eba76d256a1b7
ashish_nargade <ashish_nargade@ARPGL000908.persistent.co.in>
2020-11-09 12:04:21 +0530

    Modify method

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import string
 import sys
 # This is a sample Python script.
 
@@ -5,11 +6,11 @@ import sys
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(env,lambda):
+def print_hi(env: string, lname: string):
     # Use a breakpoint in the code line below to debug your script.
-    print('Hi, There')
+	print('Hi, There')
 	print('Arguments from the Jenkins Job: ' + str(sys.argv[1]), str(sys.argv[2]))
-
+	print('Method Parameters: ' + env +' '+lname)
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
